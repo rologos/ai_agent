65fb463dc95ad575b9e5c35c2208a87a065cf336
rologos <170283129+rologos@users.noreply.github.com>
2025-09-18 18:09:11 -0500

    refactor

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ from google import genai
 from functools import reduce
 
 def main(*args):
-    if len(args) < 1:
-        print("no prompt provided")
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
-    joinedargs = reduce(lambda x,y: x + " " + y,args,"")
+    user_prompt = " ".join(args)
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model='gemini-2.0-flash', contents=joinedargs
+        model='gemini-2.0-flash', contents=user_prompt
     )
     
     print(response.text)
@@ -21,4 +25,4 @@ def main(*args):
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
-    main(*sys.argv[1:])
+    main()

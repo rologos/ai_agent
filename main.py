9d6a28e564af70db39907affcfae349e46e9392b
rologos <170283129+rologos@users.noreply.github.com>
2025-09-18 17:39:04 -0500

    sys argument prompt

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+
+def main(*args):
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model='gemini-2.0-flash', contents=args
+    )
+
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count + 13}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":

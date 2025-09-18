d4186a87afc1021a8283959c18a9a8c8f47b9186
rologos <170283129+rologos@users.noreply.github.com>
2025-09-18 18:08:27 -0500

    fix args

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
-
+from functools import reduce
 
 def main(*args):
+    if len(args) < 1:
+        print("no prompt provided")
+        sys.exit(1)
+    joinedargs = reduce(lambda x,y: x + " " + y,args,"")
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model='gemini-2.0-flash', contents=args
+        model='gemini-2.0-flash', contents=joinedargs
     )
-
+    
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count + 13}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-
 if __name__ == "__main__":
-    main()
+    main(*sys.argv[1:])

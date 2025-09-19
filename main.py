34b868e1fab9182a2a7e13f0991eaa55e6d2ff5a
rologos <170283129+rologos@users.noreply.github.com>
2025-09-18 20:02:17 -0500

    add --verbose option

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from functools import reduce
+from google.genai import types
 
 def main(*args):
     args = sys.argv[1:]
@@ -12,17 +13,28 @@ def main(*args):
         print('\nUsage: python main.py "your prompt here"')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
-    user_prompt = " ".join(args)
+
+    usernameswitch = False
+    if args[-1] == "--verbose":
+        user_prompt = " ".join(args[:-1])
+        usernameswitch = True
+    else:
+        user_prompt = " ".join(args)
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
     response = client.models.generate_content(
-        model='gemini-2.0-flash', contents=user_prompt
+        model='gemini-2.0-flash', contents=messages,
     )
     
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count + 13}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if usernameswitch:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count + 13}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()

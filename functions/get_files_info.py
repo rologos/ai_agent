f659216b5befccb47517735eb71f7e8dc46a3d26
rologos <170283129+rologos@users.noreply.github.com>
2025-09-20 23:30:14 -0500

    refactor

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,23 +1,23 @@
 import os
-import pathlib
+
 
 def get_files_info(working_directory, directory="."):
-    joined_path = os.path.abspath(os.path.join(working_directory, directory))
-    working_path = os.path.abspath(working_directory) 
-    if not joined_path.startswith(working_path + os.sep) and joined_path != working_path:
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.abspath(os.path.join(working_directory, directory))
+    if not target_dir.startswith(abs_working_dir):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-
-    if not os.path.isdir(joined_path):
-        return f'Error: "{directory}" is not a directory' 
-
-    contents = os.listdir(joined_path)
-
-    list_str = []
-    for item in contents:
-        p = os.path.join(joined_path,item)
-        if os.path.isfile(p):
-            list_str.append(f" - {item}: file_size={os.path.getsize(p)} bytes, is_dir=False")
-        else:
-            list_str.append(f" - {item}: file_size=12 bytes, is_dir=True")
-
-    return "\n".join(list_str)
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
+    try:
+        files_info = []
+        for filename in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, filename)
+            file_size = 0
+            is_dir = os.path.isdir(filepath)
+            file_size = os.path.getsize(filepath)
+            files_info.append(
+                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            )
+        return "\n".join(files_info)
+    except Exception as e:
+        return f"Error listing files: {e}"

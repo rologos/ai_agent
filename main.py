eb778c79c23b13303db90fcd549aed74725f330f
rologos <170283129+rologos@users.noreply.github.com>
2025-09-23 13:40:44 -0500

    give system prompt to ai agent

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from functools import reduce
 from google.genai import types
+import config
 
 def main(*args):
     load_dotenv()
@@ -39,6 +40,7 @@ def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=config.SYSTEM_PROMPT),
     )
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)

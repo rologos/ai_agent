841f4b01354215b929d9722c3cd6547b08aa131a
rologos <170283129+rologos@users.noreply.github.com>
2025-09-22 22:51:50 -0500

    add read file function

---
+++ functions/get_file_content.py
@@ -0,0 +1,26 @@
+import os
+import config
+
+def get_file_content(working_directory,file_path):
+
+    joined_path = os.path.abspath(os.path.join(working_directory,file_path))
+    working_path = os.path.abspath(working_directory)
+
+    if not joined_path.startswith(working_path):
+        return f"Error: Cannot read \"{file_path}\" as it is outside the permitted working directory"
+    
+    if not os.path.isfile(joined_path):
+        return f"Error: File not found or is not regular file:\"{file_path}\""
+
+    print(config.MAX_CHARS)
+
+    try:
+        with open(joined_path, "r") as f:
+            file_content_string = f.read(config.MAX_CHARS)
+        if len(file_content_string) ==10000:
+            file_content_string = file_content_string + f"[...File \"{file_path}\" truncated at 10000 characters]"
+        return file_content_string
+    except Exception as e:
+        return f"Error listing files: {e}"
+
+
--- tests.py
+++ tests.py
@@ -1,34 +1,27 @@
 import unittest
 from functions.get_files_info import get_files_info 
+from functions.get_file_content import get_file_content 
 
 class TestGetFiles(unittest.TestCase):
 
     def test_get_parent(self):
         print(f"Result for \'../\' directory:")
-        result = get_files_info("calculator", "../")
+        result = get_file_content("calculator", "main.py")
         print(result)
-        self.assertIn("Error:",result)
 
     def test_current(self):
         print(f"Result for current directory:")
-        result = get_files_info("calculator", ".")
+        result = get_file_content("calculator", "pkg/calculator.py")
         print(result)
-        self.assertIn("tests.py",result)
-        self.assertIn("main.py",result)
-        self.assertIn("is_dir=False",result)
-        self.assertIn("is_dir=True",result)
 
     def test_pkg(self):
         print(f"Result for \'pkg\' directory:")
-        result = get_files_info("calculator", "pkg")
-        print(result)
-        self.assertIn("render.py",result)
-        self.assertIn("calculator.py",result)
+        result = get_file_content("calculator", "/bin/cat")
+        self.assertIn("Error:",result)
 
     def test_bin(self):
         print(f"Result for \'/bin\' directory:")
-        result = get_files_info("calculator", "/bin")
-        print(result)
+        result = get_file_content("calculator", "pkg/does_not_exist.py")
         self.assertIn("Error:",result)
 
 if __name__ == "__main__":
